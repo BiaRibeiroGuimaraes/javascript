77a3dee843294516879382eb8852c97203b21203
BiaRibeiroGuimaraes <107880136+BiaRibeiroGuimaraes@users.noreply.github.com>
2024-08-14 11:35:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-alert("olá,tudo bem?")
-var hora= 
+function carregar(){
+  var msg = window.document.getElementById('msg') 
+  var img= window.document.getElementById('imagem')
+  var data = new Date() //pega a data atual
+  var hora = data.getHours()//pega a hora da data atual
+  msg.inneerHTML = `Agora sao ${hora} horas`
+  if (hora >=0 && hora <12){
+    //Bom Dia
+  img.src= 'manha.jpg'
+  } else if(hora >=12 && hora <18){
+//Boa Tarde
+    img.src = 'tarde.jpg'
+  }else {
+    //Boa noite
+    img.src='noite.jpg'
+  }
+    
+
+
+
